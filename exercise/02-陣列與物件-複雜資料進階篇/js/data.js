f49ce6fc299b5399bcf11cf9712a4eba0e5455e8
renew-key <joan19980807@gmail.com>
2024-11-29 13:16:23 +0800

    更新第二章

--- exercise/02-陣列與物件-複雜資料進階篇/js/data.js
+++ exercise/02-陣列與物件-複雜資料進階篇/js/data.js
@@ -1,58 +1,58 @@
 var data = [
-    {
-        idx: 1,
-        name: "小豬",
-        money: 12000,
-        age: 12,
-        sex: "boy"
-    },
-    {
-        idx: 2,
-        name: "小烏龜",
-        money: 52000,
-        age: 20,
-        sex: "girl"
-    },
-    {
-        idx: 3,
-        name: "小鳥",
-        money: 150,
-        age: 16,
-        sex: "boy"
-    },
-    {
-        idx: 4,
-        name: "小貓",
-        money: 360000,
-        age: 35,
-        sex: "boy"
-    },
-    {
-        idx: 5,
-        name: "小狗",
-        money: 650,
-        age: 19,
-        sex: "boy"
-    },
-    {
-        idx: 6,
-        name: "鱷魚",
-        money: 9999,
-        age: 40,
-        sex: "girl"
-    },
-    {
-        idx: 7,
-        name: "龍蝦",
-        money: 10,
-        age: 2,
-        sex: "girl"
-    },
-    {
-        idx: 8,
-        name: "海藻",
-        money: 500,
-        age: 30,
-        sex: "boy"
-    }
-]
+  {
+    idx: 1,
+    name: 'Jack',
+    money: 12000,
+    age: 12,
+    sex: 'Male',
+  },
+  {
+    idx: 2,
+    name: 'Amy',
+    money: 52000,
+    age: 20,
+    sex: 'Female',
+  },
+  {
+    idx: 3,
+    name: 'John',
+    money: 150,
+    age: 16,
+    sex: 'Male',
+  },
+  {
+    idx: 4,
+    name: 'Alex',
+    money: 360000,
+    age: 35,
+    sex: 'Male',
+  },
+  {
+    idx: 5,
+    name: 'Tom',
+    money: 650,
+    age: 19,
+    sex: 'Male',
+  },
+  {
+    idx: 6,
+    name: 'Jane',
+    money: 9999,
+    age: 40,
+    sex: 'Female',
+  },
+  {
+    idx: 7,
+    name: 'Emma',
+    money: 10,
+    age: 2,
+    sex: 'Female',
+  },
+  {
+    idx: 8,
+    name: 'Ben',
+    money: 500,
+    age: 30,
+    sex: 'Male',
+  },
+];
